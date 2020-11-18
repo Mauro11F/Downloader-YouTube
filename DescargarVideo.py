562b74d3a8340575e92206ffce3825455cde7037
Mauro_Franco <49313625+Mauro11F@users.noreply.github.com>
2020-11-18 09:40:38 -0300

    subido
    Carpeta "Videos" debe ser creada

---
+++ DescargarVideo.py
@@ -0,0 +1,27 @@
+# Descargar videos de YOUTUBE
+import pafy
+
+print ("                                                                                                            ")
+print ("  ██████╗  ██████╗ ██╗    ██╗███╗   ██╗██╗      ██████╗  █████╗ ██████╗       ███╗   ██╗ ██████╗ ██╗    ██╗ ")
+print ("  ██╔══██╗██╔═══██╗██║    ██║████╗  ██║██║     ██╔═══██╗██╔══██╗██╔══██╗      ████╗  ██║██╔═══██╗██║    ██║ ")
+print ("  ██║  ██║██║   ██║██║ █╗ ██║██╔██╗ ██║██║     ██║   ██║███████║██║  ██║█████╗██╔██╗ ██║██║   ██║██║ █╗ ██║ ")
+print ("  ██║  ██║██║   ██║██║███╗██║██║╚██╗██║██║     ██║   ██║██╔══██║██║  ██║╚════╝██║╚██╗██║██║   ██║██║███╗██║ ")
+print ("  ██████╔╝╚██████╔╝╚███╔███╔╝██║ ╚████║███████╗╚██████╔╝██║  ██║██████╔╝      ██║ ╚████║╚██████╔╝╚███╔███╔╝ ")
+print ("  ╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝       ╚═╝  ╚═══╝ ╚═════╝  ╚══╝╚══╝  ")
+print ("                                                                                                            ")
+
+url = input("Pegar URL del video: ")
+
+video = pafy.new(url)
+
+streams = video.streams
+
+for i in streams:
+    print(i)
+
+best = video.getbest()
+print(best.resolution, best.extension)
+
+# .download(filepath="ubicaion")  Donde se descarga(guarda) el Video...
+best.download(filepath="C:/Users/Mauro/Desktop/AppDowload-YouTube/Videos")
+print("Descarga finalizada...")
